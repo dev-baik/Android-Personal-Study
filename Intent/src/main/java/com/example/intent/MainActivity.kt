3b6cbaa438829f168e6750b41734a69562f1222a
wlsdn9297 <wlsdn9297@naver.com>
2022-11-19 13:57:52 +0900

    액티비티 및 인텐트

---
+++ Intent/src/main/java/com/example/intent/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.intent
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.intent.databinding.ActivityMainBinding
+
+private val LOG_TAG = MainActivity::class.java.simpleName
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    companion object {
+        const val EXTRA_MESSAGE = "extra_message"
+        const val TEXT_REQUEST = 1
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == TEXT_REQUEST) {
+            if(resultCode == RESULT_OK) {
+                val reply = data?.getStringExtra(SecondActivity.EXTRA_REPLY)
+                binding.textHeaderReply.visibility = View.VISIBLE
+                binding.textMessageReply.text = reply
+                binding.textMessageReply.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    fun launchSecondActivity(view: View) {
+        Log.d(LOG_TAG, "Button clicked!")
+        val intent = Intent(applicationContext, SecondActivity::class.java)
+        val message = binding.editTextMain.text.toString()
+        intent.putExtra(EXTRA_MESSAGE, message)
+        startActivityForResult(intent, TEXT_REQUEST)
+    }
+}
---
+++ Intent/src/main/java/com/example/intent/SecondActivity.kt
@@ -0,0 +1,32 @@
+package com.example.intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.example.intent.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    private val binding by lazy { ActivitySecondBinding.inflate(layoutInflater) }
+
+    companion object {
+        const val EXTRA_REPLY = "extra_reply"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
+        binding.textMessage.text = message
+    }
+
+    fun returnReply(view: View) {
+        val reply = binding.editTextSecond.text.toString()
+        val replyIntent = Intent()
+        replyIntent.putExtra(EXTRA_REPLY, reply)
+        setResult(RESULT_OK, replyIntent)
+        finish()
+    }
+}
