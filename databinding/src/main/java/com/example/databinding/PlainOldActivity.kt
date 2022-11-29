65d09a1482a290e5e5e86088a88a6387dad97a94
wlsdn9297 <wlsdn9297@naver.com>
2022-11-29 20:07:38 +0900

    Observing data

--- databinding/src/main/java/com/example/databinding/PlainOldActivity.kt
+++ databinding/src/main/java/com/example/databinding/PlainOldActivity.kt
@@ -16,17 +16,8 @@
 
 package com.example.databinding
 
-import android.content.Context
-import android.content.res.ColorStateList
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.widget.ImageViewCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.example.databinding.databinding.PlainActivityBinding
@@ -41,55 +32,10 @@ class PlainOldActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         var binding : PlainActivityBinding = DataBindingUtil.setContentView(this, R.layout.plain_activity)
-        setContentView(binding.root)
 
-        binding.name = "Ada"
-        binding.lastName = "Lovelace"
+        binding.lifecycleOwner = this
 
-        updateLikes()
-    }
-
-    fun onLike(view: View) {
-        viewModel.onLike()
-        updateLikes()
-    }
-
-    private fun updateLikes() {
-        findViewById<TextView>(R.id.likes).text = viewModel.likes.toString()
-        findViewById<ProgressBar>(R.id.progressBar).progress =
-            (viewModel.likes * 100 / 5).coerceAtMost(100)
-        val image = findViewById<ImageView>(R.id.imageView)
-
-        val color = getAssociatedColor(viewModel.popularity, this)
-
-        ImageViewCompat.setImageTintList(image, ColorStateList.valueOf(color))
-
-        image.setImageDrawable(getDrawablePopularity(viewModel.popularity, this))
-    }
-
-    private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
-        return when (popularity) {
-            Popularity.NORMAL -> context.theme.obtainStyledAttributes(
-                intArrayOf(android.R.attr.colorForeground)
-            ).getColor(0, 0x000000)
-            Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
-            Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
-        }
-    }
-
-    private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
-        return when (popularity) {
-            Popularity.NORMAL -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_person_black_96dp)
-            }
-            Popularity.POPULAR -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            }
-            Popularity.STAR -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            }
-        }
+        binding.viewmodel = viewModel
     }
 }
